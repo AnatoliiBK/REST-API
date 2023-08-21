7ff589f65a3ed8f581f2495d2a05691ddca7e577
AnetRo <beatrice.ccq@gmail.com>
2023-08-21 17:53:37 +0300

    mes

--- schemas/contacts.js
+++ schemas/contacts.js
@@ -1,16 +1,3 @@
-// const Joi = require('joi');
-
-
-// const addSchema = Joi.object({
-//     name: Joi.string().required(),
-//     email: Joi.string().email().required(),
-//     phone: Joi.string().required(),
-//   });
-
-//   module.exports = {
-//     addSchema
-//   }
-
 const Joi = require('joi');
 
 const addSchema = Joi.object({
@@ -28,3 +15,19 @@ const addSchema = Joi.object({
   module.exports = {
     addSchema
   }
+
+// const Joi = require('joi');
+
+// const customMessages = {
+//     'any.required': 'missing required {#label} field'
+// };
+
+// const addSchema = Joi.object({
+//     name: Joi.string().required().messages(customMessages),
+//     email: Joi.string().email().required().messages(customMessages),
+//     phone: Joi.string().required().messages(customMessages)
+// });
+
+// module.exports = {
+//     addSchema
+// };
