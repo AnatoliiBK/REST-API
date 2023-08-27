302e2d35b1776e41f21a27a38a2b082514294413
AnetRo <beatrice.ccq@gmail.com>
2023-08-27 11:22:45 +0300

    FI

---
+++ config.js
@@ -0,0 +1,5 @@
+const DB_HOST = 'mongodb+srv://Anatolii:EtvurSt7qq92W4dF@cluster0.rkzhogc.mongodb.net/phonebook?retryWrites=true&w=majority'
+
+module.exports = {
+    DB_HOST,
+}
--- helpers/index.js
+++ helpers/index.js
@@ -1,7 +1,9 @@
 const HttpError = require("./Http-Error");
 const ctrlWrapper = require('./ctrlWrapper');
+const handleMongooseError = require("../middlewares/handleMongooseError");
 
 module.exports = {
   HttpError,
-  ctrlWrapper
+  ctrlWrapper,
+  handleMongooseError
 };
