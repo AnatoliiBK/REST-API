c1c67805278c99e3f399b1ec63442610ba5b92a5
AnetRo <beatrice.ccq@gmail.com>
2023-09-02 15:16:59 +0300

    upd

--- config.js
+++ config.js
@@ -1,5 +1,6 @@
 const DB_HOST = 'mongodb+srv://Anatolii:EtvurSt7qq92W4dF@cluster0.rkzhogc.mongodb.net/db-contacts?retryWrites=true&w=majority'
-
+const SECRET_KEY = 'KdNL[TA4ej:jh1J{eAS3]sFfH*ZyFi'
 module.exports = {
     DB_HOST,
+    SECRET_KEY
 }
--- controllers/auth.js
+++ controllers/auth.js
@@ -2,6 +2,7 @@ const { User } = require('../models/user');
 const { HttpError, ctrlWrapper } = require("../helpers");
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
+// const {SECRET_KEY} = process.env;
 const {SECRET_KEY} = process.env;
 
 const register = async(req, res) => {
@@ -19,23 +20,44 @@ const register = async(req, res) => {
     })
 }
 
-const login = async(req, res) => {
-    const { email, password } = req.body;
+// const login = async(req, res) => {
+//     const { email, password } = req.body;
+//     const user = await User.findOne({email});
+//     if (!user) {
+//         throw HttpError(401, 'Email or password invalid');
+//     }
+//     const passwordCompare = await bcrypt.compare(password, user.password);
+
+//     if (!passwordCompare) {
+//         throw HttpError(401, 'Email or password invalid');
+//     }
+    
+//     const payload = {
+//         contactId: user._id,
+//     }
+
+//     const token = jwt.sign(payload, SECRET_KEY, {expiresIn: '23h'});
+//     res.json({
+//         token,
+//     })
+// }
+const login = async(req, res)=> {
+    const {email, password} = req.body;
     const user = await User.findOne({email});
-    if (!user) {
-        throw HttpError(401, 'Email or password invalid');
+    if(!user){
+        throw HttpError(401, "Email or password invalid");
     }
     const passwordCompare = await bcrypt.compare(password, user.password);
-
-    if (!passwordCompare) {
-        throw HttpError(401, 'Email or password invalid');
+    if(!passwordCompare) {
+        throw HttpError(401, "Email or password invalid");
     }
-    
+
     const payload = {
         id: user._id,
     }
 
-    const token = jwt.sign(payload, SECRET_KEY, {expiresIn: '23h'});
+    const token = jwt.sign(payload, SECRET_KEY, {expiresIn: "23h"});
+
     res.json({
         token,
     })
