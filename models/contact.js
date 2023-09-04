b168e714caa46658ccf916d4e613cd49d5ba092a
AnetRo <beatrice.ccq@gmail.com>
2023-09-05 00:38:56 +0300

    upd contacts

--- models/contact.js
+++ models/contact.js
@@ -2,26 +2,29 @@ const { Schema, model } = require("mongoose");
 const handleMongooseError = require("../helpers/handleMongooseError");
 const Joi = require("joi");
 
-const contactSchema = new Schema({
-  name: {
-    type: String,
-    required: [true, "Set name for contact"],
+const contactSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Set name for contact"],
+    },
+    email: {
+      type: String,
+    },
+    phone: {
+      type: String,
+    },
+    favorite: {
+      type: Boolean,
+      default: false,
+    },
+    owner: {
+      type: Schema.Types.ObjectId,
+      ref: "user",
+    },
   },
-  email: {
-    type: String,
-  },
-  phone: {
-    type: String,
-  },
-  favorite: {
-    type: Boolean,
-    default: false,
-  },
-  owner: {
-    type: Schema.Types.ObjectId,
-    ref: 'user',
-  }
-});
+  { versionKey: false }
+);
 
 const addSchema = Joi.object({
   name: Joi.string().required().messages({
