44ed28251d49831835bd0407468ca9b76a354876
AnetRo <beatrice.ccq@gmail.com>
2023-09-04 23:32:56 +0300

    upd auth

--- app.js
+++ app.js
@@ -1,39 +1,8 @@
-// const express = require('express')
-// const logger = require('morgan')
-// const cors = require('cors')
-
-
-// const authRouter = require('./routes/api/auth');
-// const contactsRouter = require('./routes/api/contacts')
-
-// const app = express()
-
-// const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short'
-
-// app.use(logger(formatsLogger))
-// app.use(cors())
-// app.use(express.json())
-
-// app.use('/api/auth', authRouter)
-// app.use('/api/contacts', contactsRouter)
-
-// app.use((req, res) => {
-//   res.status(404).json({ message: 'Not found' })
-// })
-
-// app.use((err, req, res, next) => {
-  
-//   const { status = 500, message = "Server Error" } = err;
-//     res.status(status).json({ message });
-// })
-
-// module.exports = app
-
 const express = require("express");
 const logger = require("morgan");
 const cors = require("cors");
 
-const authRouter = require("./routes/auth"); // зміни це
+const authRouter = require("./routes/auth");
 const contactsRouter = require("./routes/api/contacts");
 
 const app = express();
@@ -44,7 +13,7 @@ app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
 
-app.use("/users", authRouter); // зміни це
+app.use("/users", authRouter);
 app.use("/api/contacts", contactsRouter);
 
 app.use((req, res) => {
--- server.js
+++ server.js
@@ -1,22 +1,3 @@
-// const mongoose = require('mongoose');
-// const { DB_HOST } = require('./config');
-
-// const app = require('./app')
-
-// mongoose.set('strictQuery', true)
-
-// mongoose.connect(DB_HOST)
-//   .then(() => {
-//     app.listen(3000);
-//     console.log("Database connection successful");
-
-//   })
-//   .catch(error => {
-//     console.log(error.message);
-//     process.exit(1);
-//   })
-
-
 const mongoose = require("mongoose");
 
 require("dotenv").config({ override: true, debug: true });
