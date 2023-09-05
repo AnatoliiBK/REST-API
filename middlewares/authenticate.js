655d1aca669af3995826c351ca22ebe26d94f7cc
AnetRo <beatrice.ccq@gmail.com>
2023-09-05 21:07:50 +0300

    UPD

--- middlewares/authenticate.js
+++ middlewares/authenticate.js
@@ -1,7 +1,7 @@
 const jwt = require("jsonwebtoken");
 const { User } = require("../models/user");
-const HttpError = require("../helpers");
-const SECRET_KEY = process.env;
+const { HttpError } = require("../helpers");
+const { SECRET_KEY } = process.env;
 
 const authenticate = async (req, res, next) => {
   const { authorization = "" } = req.headers;
@@ -12,6 +12,7 @@ const authenticate = async (req, res, next) => {
   try {
     const { id } = jwt.verify(token, SECRET_KEY);
     const user = await User.findById(id);
+    console.log(user);
     if (!user) {
       next(HttpError(401, "Not authorized"));
     }
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,20 +1,42 @@
 const express = require("express");
-const { validateBody, isValidId, validateStatus, authenticate  } = require('../../middlewares');
+const {
+  validateBody,
+  isValidId,
+  validateStatus,
+  authenticate,
+} = require("../../middlewares");
 
 const { schemas } = require("../../models/contact");
 const router = express.Router();
 const ctrl = require("../../controllers/contacts");
 
-router.get("/", ctrl.listContacts);
+router.get("/", authenticate, ctrl.listContacts);
 
 router.get("/:contactId", authenticate, isValidId, ctrl.getContactById);
 
-router.post("/", authenticate, validateBody(schemas.addSchema), ctrl.addContact);
+router.post(
+  "/",
+  authenticate,
+  validateBody(schemas.addSchema),
+  ctrl.addContact
+);
 
 router.delete("/:contactId", authenticate, isValidId, ctrl.removeContact);
 
-router.put("/:contactId", authenticate, isValidId, validateBody(schemas.addSchema), ctrl.updateContact);
+router.put(
+  "/:contactId",
+  authenticate,
+  isValidId,
+  validateBody(schemas.addSchema),
+  ctrl.updateContact
+);
 
-router.patch("/:contactId/favorite", authenticate, isValidId, validateStatus(schemas.updateFavoriteSchema), ctrl.updateStatusContact);
+router.patch(
+  "/:contactId/favorite",
+  authenticate,
+  isValidId,
+  validateStatus(schemas.updateFavoriteSchema),
+  ctrl.updateStatusContact
+);
 
 module.exports = router;
