a18b2d2819fa86c45f63c0404f46577d77c016a4
AnetRo <beatrice.ccq@gmail.com>
2023-09-05 20:13:13 +0300

    Update auth.js

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,51 +1,50 @@
-const { User } = require('../models/user');
+const { User } = require("../models/user");
 const { HttpError, ctrlWrapper } = require("../helpers");
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
-const {SECRET_KEY} = process.env;
+const { SECRET_KEY } = process.env;
 
-const register = async(req, res) => {
-    const {email, password} = req.body;
-    const user = await User.findOne({ email });
-    if (user) {
-        throw HttpError(409, 'Email in use');
-    }
+const register = async (req, res) => {
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
+  if (user) {
+    throw HttpError(409, "Email in use");
+  }
 
-    const hashPassword = await bcrypt.hash(password, 10);
+  const hashPassword = await bcrypt.hash(password, 10);
 
-    const newUser = await User.create({...req.body, password: hashPassword});
-   
-    res.status(201).json({
-        user: { email: newUser.email, subscription: newUser.subscription },
-      });
-}
+  const newUser = await User.create({ ...req.body, password: hashPassword });
 
+  res.status(201).json({
+    user: { email: newUser.email, subscription: newUser.subscription },
+  });
+};
 
-const login = async(req, res)=> {
-    const {email, password} = req.body;
-    const user = await User.findOne({email});
-    if(!user){
-        throw HttpError(401, "Email or password invalid");
-    }
-    const passwordCompare = await bcrypt.compare(password, user.password);
-    if(!passwordCompare) {
-        throw HttpError(401, "Email or password invalid");
-    }
+const login = async (req, res) => {
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
+  if (!user) {
+    throw HttpError(401, "Email or password wrong");
+  }
+  const passwordCompare = await bcrypt.compare(password, user.password);
+  if (!passwordCompare) {
+    throw HttpError(401, "Email or password wrong");
+  }
 
-    const payload = {
-        id: user._id,
-    }
+  const payload = {
+    id: user._id,
+  };
 
-    const token = jwt.sign(payload, SECRET_KEY, {expiresIn: "23h"});
+  const token = jwt.sign(payload, SECRET_KEY, { expiresIn: "23h" });
 
-    res.json({
-        token,
-        user: { email: user.email, subscription: user.subscription },
-      });
-}
+  res.json({
+    token,
+    user: { email: user.email, subscription: user.subscription },
+  });
+};
 
 module.exports = {
-    register: ctrlWrapper(register),
-    login: ctrlWrapper(login)
-}
+  register: ctrlWrapper(register),
+  login: ctrlWrapper(login),
+};
