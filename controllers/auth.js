af50c008d524229f09ae3f068a75b25cd9267b90
AnetRo <beatrice.ccq@gmail.com>
2023-09-07 00:06:06 +0300

    Update auth.js

--- controllers/auth.js
+++ controllers/auth.js
@@ -55,7 +55,7 @@ const getCurrent = async (req, res) => {
 const logout = async (req, res) => {
   const { _id } = req.user;
   await User.findByIdAndUpdate(_id, { token: "" });
-  res.status(204);
+  res.status(204).json();
 };
 
 module.exports = {
